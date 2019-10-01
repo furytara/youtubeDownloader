e1b9951b3c82b67e075d9425c5ccc303352c8c10
doanhho <doanhhnqt74@gmail.com>
2019-10-01 17:13:11 +0700

    using build kite variable to return build result

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -1,3 +1,9 @@
 Write-Output "--- Execute in build.ps1";
 
-throw "Build Failed. see log err for details";
+if (${Env:BUILD_EXIT} -eq 1) {
+	Write-Output "--- Build failure";
+	throw "Build Failed. see log err for details";
+}
+
+Write-Output "--- Build success";
+
